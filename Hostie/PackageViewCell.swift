18ea2530566851b33128acdd775c97444322df1a
Mojtaba Fazel <fazeli.mojtaba@gmail.com>
2021-02-13 10:41:47 -0500

    initial commit

---
+++ Hostie/PackageViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  PackageViewCell.swift
+//  Hostie
+//
+//  Created by student on 2021-02-12.
+//
+
+import UIKit
+
+class PackageViewCell: UICollectionViewCell {
+    static let identifier = "PackageViewCell"
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
---
+++ Hostie/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Hostie
+//
+//  Created by student on 2021-01-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    
+    
+    private let collectionView = UICollectionView (
+        frame: .zero,
+        collectionViewLayout: UICollectionViewLayout()
+    )
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectinViewCell()
+    }
+
+}
+
